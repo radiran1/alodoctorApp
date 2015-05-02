953549cb956bd2ec66826a5f67b2914bfb2a693e
radiran1 <radiran1@hotmail.com>
2015-05-02 16:09:25 +0430

    Added bower lodash and edited note

--- www/app/controllers/note-ctrl.js
+++ www/app/controllers/note-ctrl.js
@@ -3,12 +3,12 @@
     angular.module('alodoctorApp').controller('NoteCtrl', ['$stateParams', 'alodoctorApi', NoteCtrl]);
     function NoteCtrl($stateParams, alodoctorApi) {
         var note = this;
-        var noteId = Number($stateParams.Id);
-        var response = alodoctorApi.getNote();
-        note = response;
-        console.log("Note is :", note);
-        console.log("NoteId is :", noteId);
-        console.log("Id is :", note.Id);
-        console.log("Name is :", note.Name);
+        note.Id = Number($stateParams.Id);
+        var notes = alodoctorApi.getNotes();
+
+        var data = _.chain(notes.Notes)
+            .find({ "Id": note.Id })
+            .value();
+        note.Name = data.Name;
     }
 })();
