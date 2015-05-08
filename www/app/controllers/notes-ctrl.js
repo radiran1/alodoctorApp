c4d3033db3ad07782994f6c216f23dab9d2bc4fc
radiran1 <radiran1@hotmail.com>
2015-05-08 19:50:47 +0430

    Edited Intro Style and Notes

--- www/app/controllers/notes-ctrl.js
+++ www/app/controllers/notes-ctrl.js
@@ -3,9 +3,8 @@
 	angular.module('alodoctorApp').controller('NotesCtrl', ['$stateParams', 'alodoctorApi', NotesCtrl]);
 	function NotesCtrl($stateParams, alodoctorApi) {
 		var notes = this;
-		var clinicId = Number($stateParams.clinicId);
-		notes.ClinicId = clinicId;
-		alodoctorApi.getNotes(clinicId).then(function (data) {
+		notes.ClinicId = Number($stateParams.clinicId);
+		alodoctorApi.getNotes(notes.ClinicId).then(function (data) {
 			notes.Notes = data.Notes;
 		});
 	}
