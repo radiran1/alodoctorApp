cd6dd09769e89c76527927ee686f65b8d5374624
radiran1 <radiran1@hotmail.com>
2015-05-14 12:10:20 +0430

    Edit Directory Structure

--- www/app/app.js
+++ www/app/app.js
@@ -1,95 +1,95 @@
-  angular.module('alodoctorApp', ['ngCordova','ngSanitize', 'ionic'])
+angular.module('alodoctorApp', ['ngCordova','ngSanitize', 'ionic'])
 
-  .run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-      if (window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      }
-      if (window.StatusBar) {
-        StatusBar.styleDefault();
-      }
-    });
+.run(function ($ionicPlatform) {
+  $ionicPlatform.ready(function () {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+.config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('intro', {
+    url: '/',
+    templateUrl: 'app/intro/intro.html'
   })
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-
-    .state('intro', {
-      url: '/',
-      templateUrl: 'app/templates/intro.html'
-    })
-    .state('app', {
-      url: "/app",
-      abstract: true,
-      templateUrl: "app/templates/menu.html"
-    })
-    .state('home', {
-      url: "/home",
-      templateUrl: "app/templates/home.html"
-    })
-    .state('app.notes', {
-      url: "/notes/:clinicId",
-      views: {
-        'menuContent': {
-          templateUrl: "app/templates/notes.html"
-        }
-      }
-    })
-    .state('app.questions', {
-      url: "/questions/:clinicId",
-      views: {
-        'menuContent': {
-          templateUrl: "app/templates/questions.html"
-        }
-      }
-    })
-    .state('app.doctors', {
-      url: "/doctors/:clinicId",
-      views: {
-        'menuContent': {
-          templateUrl: "app/templates/doctors.html"
-        }
-      }
-    })
-    .state('app.popularnotes', {
-      url: "/popularnotes/:clinicId",
-      views: {
-        'menuContent': {
-          templateUrl: "app/templates/popularnotes.html"
-        }
-      }
-    })
-    .state('app.note', {
-      url: "/note/:Id",
-      views: {
-        'menuContent': {
-          templateUrl: "app/templates/note.html"
-        }
-      }
-    })
-
-    .state('app.doctor', {
-      url: "/doctor/:Id",
-      views: {
-        'menuContent': {
-          templateUrl: "app/templates/doctor.html"
-        }
-      }
-    })
-    .state('app.question', {
-      url: "/question/:Id",
-      views: {
-        'menuContent': {
-          templateUrl: "app/templates/question.html"
-        }
-      }
-    })
-    .state('app.demo', {
-      url: "/demo",
-      views: {
-        'menuContent': {
-          templateUrl: "app/templates/demo.html"
-        }
-      }
-    });
-    $urlRouterProvider.otherwise('/');
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "app/layout/menu.html"
   })
+  .state('home', {
+    url: "/home",
+    templateUrl: "app/home/home.html"
+  })
+  .state('app.notes', {
+    url: "/notes/:clinicId",
+    views: {
+      'menuContent': {
+        templateUrl: "app/notes/notes.html"
+      }
+    }
+  })
+  .state('app.questions', {
+    url: "/questions/:clinicId",
+    views: {
+      'menuContent': {
+        templateUrl: "app/questions/questions.html"
+      }
+    }
+  })
+  .state('app.doctors', {
+    url: "/doctors/:clinicId",
+    views: {
+      'menuContent': {
+        templateUrl: "app/doctors/doctors.html"
+      }
+    }
+  })
+  .state('app.popularnotes', {
+    url: "/popularnotes/:clinicId",
+    views: {
+      'menuContent': {
+        templateUrl: "app/notes/popularnotes.html"
+      }
+    }
+  })
+  .state('app.note', {
+    url: "/note/:Id",
+    views: {
+      'menuContent': {
+        templateUrl: "app/notes/note.html"
+      }
+    }
+  })
+
+  .state('app.doctor', {
+    url: "/doctor/:Id",
+    views: {
+      'menuContent': {
+        templateUrl: "app/doctors/doctor.html"
+      }
+    }
+  })
+  .state('app.question', {
+    url: "/question/:Id",
+    views: {
+      'menuContent': {
+        templateUrl: "app/questions/question.html"
+      }
+    }
+  })
+  .state('app.demo', {
+    url: "/demo",
+    views: {
+      'menuContent': {
+        templateUrl: "app/home/demo.html"
+      }
+    }
+  });
+  $urlRouterProvider.otherwise('/');
+})
